f03529d5e9a2e0ab364aca801dbc38a649847750
rezzouq <mohammed.rezzouq@siway.ma>
2022-06-21 17:48:25 +0100

    ci(Create register route controller and we used invoke method which is the default of a controller):

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TodoListController;
 use Illuminate\Support\Facades\Route;
@@ -26,3 +27,10 @@ Route::post('task/completed', [TaskController::class, 'store'])->name('task.stor
 // Route::get('task', [TaskController::class, 'index'])->name('task.index');
 // Route::post('task', [TaskController::class, 'store'])->name('task.store');
 // Route::delete('task/{task}', [TaskController::class, 'destroy'])->name('task.destroy');
+
+
+
+
+///////////////register routes
+
+Route::post('/register', RegisterController::class)->name('user.register');
