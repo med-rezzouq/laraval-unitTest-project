eb33b71afecc56f308ecae5512873070f712993f
rezzouq <mohammed.rezzouq@siway.ma>
2022-06-21 14:27:23 +0100

    ci(created new crud item  'task' and tests and routes and factory): every todolist has tasks

---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Task;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TaskTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_fetch_all_tasks_of_a_todo_list()
+    {
+
+        //preparation
+        $task = $this->createTask();
+
+        //action
+
+        $response = $this->getJson(route('task.index'))->assertOk()->json();
+
+        //assertion
+
+        $this->assertEquals(1, count($response));
+        $this->assertEquals($task->title, $response[0]['title']);
+    }
+
+    public function test_store_a_task_of_a_todo_list()
+    {
+        $task = Task::factory()->make();
+
+        $this->postJson(route('task.store'), ['title' => $task->title])->assertCreated()->json();
+
+        $this->assertDatabaseHas('tasks', ['title' => $task->title]);
+    }
+
+
+    public function test_delete_a_task_from_database()
+    {
+        $task = $this->createTask();
+        $this->deleteJson(route('task.destroy', $task->id))->assertNoContent();
+        $this->assertDatabaseMissing('tasks', ['title' => $task->title]);
+    }
+}
